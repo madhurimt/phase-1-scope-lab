a423dc3077fd81cc32a72a3ee1f22d585e704b91
madhuri maniktahla <madhuri.maniktahla@gmail.com>
2023-08-22 17:16:29 +1000

    passing test cases

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName="bob";
+var bestCustomer;
+const leastFavoriteCustomer="abc";
+function name(){
+    return customerName;
+}
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer= "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer= "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="abcd";
+    return leastFavoriteCustomer;
+}
